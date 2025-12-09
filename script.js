5009b96510ece7fb762f347e98c74db0fd973b49
Belen Morales <156867706+belamore@users.noreply.github.com>
2025-12-09 14:19:28 -0800

    Update script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const images = ['image1.png', 'image2.png', 'image3.png', 'image4.png', 'image5.png', 'image6.png', 'image7.png', 'image8.png', 'image9.png', 'image10.png',];
+let currentIndex = 0;
+
+const previousBtn = document.getElementById('previousBtn');
+
+const nextBtn = document.getElementById('nextBtn');
+
+const image = document.getElementById('image');
+
+previousBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    image.src = images[currentIndex];
+});
+nextBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    image.src = images[currentIndex];
+
+}); 
